716457d5f571d3eb6311dc9b8608928adcefcc5c
Tariq3654467 <janjuatariq7614@gmail.com>
2026-04-09 22:51:10 +0500

    new

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,9 @@
+import Navbar from '../components/Navbar'
 import './globals.css'
 
 export const metadata = {
-  title: 'FreeConvert Replica',
-  description: 'Convert files online for free',
+  title: 'FreeConvert | File Converter',
+  description: 'Easily convert files from one format to another online.',
 }
 
 export default function RootLayout({
@@ -13,13 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <nav className="navbar">
-          <div className="logo">FreeConvert Replica</div>
-          <div className="nav-links">
-            <a href="/login">Login</a>
-            <a href="/signup" className="btn-primary">Sign Up</a>
-          </div>
-        </nav>
+        <Navbar />
         {children}
       </body>
     </html>
